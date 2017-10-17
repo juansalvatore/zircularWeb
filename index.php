5d44ebd9c6958cd84a787b4ef36a0db9cb280d26
Juan Salvatore <juansalvatore@live.com.ar>
2017-10-17 09:42:32 -0300

    Create portfolio display

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ortiz León Architects</title>
+    <link rel="stylesheet" href="styles/index-style.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/img-index-controller.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body id="body">
+    <div class="container">
+      <div class="col-one">
+        <div id="one" class="one">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="two" class="two">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="three" class="three">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="four" class="four">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+      </div>
+
+      <div class="col-two">
+        <div id="five" class="five">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="six" class="six">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="seven" class="seven">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="eight" class="eight">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+      </div>
+      <div class="col-three">
+        <div id="nine" class="nine">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="ten" class="ten">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="eleven" class="eleven">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="twelve" class="twelve">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+      </div>
+      <div class="col-four">
+        <div id="thirteen" class="thirteen">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="fourteen" class="fourteen">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="fifteen" class="fifteen">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+
+        <div id="sixteen" class="sixteen">
+          <h1>Titulo principal</h1>
+          <h3>Subtitulo</h3>
+          <img class="logo" src="img/house.png" alt="">
+          <p>Titulo</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ js/img-index-controller.js
@@ -0,0 +1,48 @@
+
+$(function()
+{
+  $('#one').hover(function() // on hover
+  {
+    $('body').css('background-image', "url(../img/building-1.jpg)");
+  },
+  function()
+  {
+    $('body').css('background-image', "url(../img/spain-travel.jpg)");
+  })
+
+  $('#two').hover(function() // on hover
+  {
+    $('body').css('background-image', "url(../img/2.jpg)");
+  },
+  function()
+  {
+    $('body').css('background-image', "url(../img/spain-travel.jpg)");
+  })
+
+  $('#three').hover(function() // on hover
+  {
+    $('body').css('background-image', "url(../img/3.jpg)");
+  },
+  function()
+  {
+    $('body').css('background-image', "url(../img/spain-travel.jpg)");
+  })
+
+  $('#four').hover(function() // on hover
+  {
+    $('body').css('background-image', "url(../img/4.jpg)");
+  },
+  function()
+  {
+    $('body').css('background-image', "url(../img/spain-travel.jpg)");
+  })
+
+  $('#five').hover(function() // on hover
+  {
+    $('body').css('background-image', "url(../img/5.jpg)");
+  },
+  function()
+  {
+    $('body').css('background-image', "url(../img/spain-travel.jpg)");
+  })
+})
